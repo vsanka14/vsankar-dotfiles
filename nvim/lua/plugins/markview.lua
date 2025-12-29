87d938cf92526f1ed44a70ceb5dda73f867bff5a
Vasisht Shankar <vsanka14@asu.edu>
2025-12-29 10:28:57 -0800

    markdown preview plugin instead of custom stuff

---
+++ nvim/lua/plugins/markview.lua
@@ -0,0 +1,8 @@
+-- Markview: Markdown, HTML, LaTeX, Typst & YAML previewer
+-- https://github.com/OXY2DEV/markview.nvim
+
+---@type LazySpec
+return {
+  "OXY2DEV/markview.nvim",
+  lazy = false, -- Don't lazy load - plugin is already lazy-loaded internally
+}
