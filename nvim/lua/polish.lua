dfe25a5d3b2cbdef80707d385fb30ad28c7c0a87
Vasisht Shankar <vsanka14@asu.edu>
2025-12-29 10:35:00 -0800

    auto-update neo-tree on git stuff

--- nvim/lua/polish.lua
+++ nvim/lua/polish.lua
@@ -1,3 +1,14 @@
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+
+-- Auto-refresh Neo-tree git status when Neovim regains focus
+-- This fixes stale git status after external git operations (push, commit, etc.)
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold" }, {
+  pattern = "*",
+  callback = function()
+    if package.loaded["neo-tree.sources.manager"] then
+      require("neo-tree.sources.manager").refresh("filesystem")
+    end
+  end,
+})
