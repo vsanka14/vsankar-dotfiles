f3bfd66b48b9e69b873b4009a136b1f6018d897b
Vasisht Shankar <vsanka14@asu.edu>
2025-12-29 04:21:38 -0800

    undo padding

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -67,10 +67,10 @@ config.font_size = 15.0
 -- Window
 config.window_decorations = "TITLE | RESIZE" -- Native macOS title bar with traffic light buttons
 config.window_padding = {
-	left = 10,
-	right = 10,
-	top = 10,
-	bottom = 10,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 
 -- Tab bar
